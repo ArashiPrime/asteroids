4ac44247b05cafee04854cac924379533eb74fa3
J.T. Kauffman <jtkauff@yahoo.com>
2025-02-09 23:12:28 -0500

    Adding player movement

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	
-	x = SCREEN_WIDTH / 2
-	y = SCREEN_HEIGHT / 2
-	player = Player(x, y)
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 	dt = 0
 
@@ -22,8 +25,14 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+					
+		updatable.update(dt)
+		
 		screen.fill("black")
-		player.draw(screen)
+		
+		for obj in drawable:
+			obj.draw(screen)
+
 		pygame.display.flip()
 
 		# limit the framerate to 60 FPS
